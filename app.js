0ebe9544b19d7cc99aa50a37d395c037ed60223a
Abeco Jean Claude <73258089+abecojc@users.noreply.github.com>
2020-12-24 13:36:29 -0500

    initial commit with top of the page

---
+++ app.js
@@ -0,0 +1,6 @@
+const tableMenu = document.getElementById("book-table")
+tableMenu.addEventListener( "click",function(){
+
+    document.body.classList.toggle('show-sliding-menu')
+   console.log()
+})
