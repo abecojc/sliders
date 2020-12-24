b26ce4d549dcadc6127199d9c99135f4fadb7499
Abeco Jean Claude <73258089+abecojc@users.noreply.github.com>
2020-12-24 15:42:55 -0500

    add toggle to side menu

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 const tableMenu = document.getElementById("book-table")
+const tableMenu2 = document.getElementById("book-table-1")
+const locations = document.getElementById("location")
+const online = document.getElementById("online")
+const deliveries = document.getElementById("delivery")
+
+
+const nav = document.getElementById("nav")
+
 tableMenu.addEventListener( "click",function(){
 
-    document.body.classList.toggle('show-sliding-menu')
-   console.log()
+    nav.classList.toggle('show-sliding-menu')
+})
+
+tableMenu2.addEventListener( "click",function(){
+
+    nav.classList.toggle('show-sliding-menu')
+})
+
+locations.addEventListener( "click",function(){
+
+    nav.classList.toggle('show-sliding-menu')
+})
+online.addEventListener( "click",function(){
+
+    nav.classList.toggle('show-sliding-menu')
+})
+deliveries.addEventListener( "click",function(){
+
+    nav.classList.toggle('show-sliding-menu')
 })
