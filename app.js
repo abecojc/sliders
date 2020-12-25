070ddbbc8cc5fa1a61e1010bef404ff9ed14b289
Abeco Jean Claude <73258089+abecojc@users.noreply.github.com>
2020-12-25 11:20:39 -0500

    add functions to top menu

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const tableMenu2 = document.getElementById("book-table-1")
 const locations = document.getElementById("location")
 const online = document.getElementById("online")
 const deliveries = document.getElementById("delivery")
-
+let headNav = document.getElementById("head-nav")
 
 const nav = document.getElementById("nav")
 
@@ -15,17 +15,21 @@ tableMenu.addEventListener( "click",function(){
 tableMenu2.addEventListener( "click",function(){
 
     nav.classList.toggle('show-sliding-menu')
+    headNav.innerHTML='Book a Table'
 })
 
 locations.addEventListener( "click",function(){
-
     nav.classList.toggle('show-sliding-menu')
+    headNav.innerHTML='Set Your Location'
+
 })
 online.addEventListener( "click",function(){
 
     nav.classList.toggle('show-sliding-menu')
+    headNav.innerHTML='Order Online'
 })
 deliveries.addEventListener( "click",function(){
 
     nav.classList.toggle('show-sliding-menu')
+    headNav.innerHTML='Get Delivery'
 })
