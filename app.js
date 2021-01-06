c1e39ed4e214459ab642f62c5828a08730d7c6ee
Abeco Jean Claude <73258089+abecojc@users.noreply.github.com>
2021-01-06 03:31:52 -0500

    add functions to top menu

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const tableMenu2 = document.getElementById("book-table-1")
 const locations = document.getElementById("location")
 const online = document.getElementById("online")
 const deliveries = document.getElementById("delivery")
-let headNav = document.getElementById("head-nav")
-
+let headNav = document.querySelector('#head-nav')
+let opaque = document.querySelector('#opaqueBg')
 const nav = document.getElementById("nav")
 
 tableMenu.addEventListener( "click",function(){
@@ -19,8 +19,10 @@ tableMenu2.addEventListener( "click",function(){
     headNav.innerHTML='Book a Table'
 })
 
+
 locations.addEventListener( "click",function(){
     nav.classList.toggle('show-sliding-menu')
+    opaque.classList.remove('hidden')
     headNav.innerHTML='Set Your Location'
 
 })
@@ -33,4 +35,13 @@ deliveries.addEventListener( "click",function(){
 
     nav.classList.toggle('show-sliding-menu')
     headNav.innerHTML='Get Delivery'
+})
+let close=
+window.addEventListener("click",function(e){
+    if(e.target!==nav){
+        nav.classList.add('hidden') && opaque.classList.add('hidden')
+        else{
+            return false
+        }
+    }
 })
